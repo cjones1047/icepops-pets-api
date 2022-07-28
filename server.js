ff6fa07b308055e1ea966cbf607642c90e37d434
cjones1047 <cjmj1047@gmail.com>
2022-07-28 14:38:15 -0500

    Seed script works, deletes all pets without owners and seeds db

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
+const petRoutes = require('./app/routes/pet_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -65,7 +66,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(petRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
